c88027ea8d1bd1d1eb9fc823a02ff30ad9056d89
William Johnson <william.johnson@bluestormdesign.co.uk>
2023-06-21 09:12:56 +0100

    Added market sectors and applications blocks as php

---
+++ components/applications.php
@@ -0,0 +1,21 @@
+<section class="my-10">
+    <div class="container">
+        <div class="grid grid-cols-12">
+            <div class="col-span-12">
+                <div class="relative">
+                    <img src="images/Applications.jpg" alt="Applications" class="w-full h-full rounded-2xl brightness-75">
+                    <div class="absolute bottom-0 left-0 p-4 md:p-8">
+                        <div class="">
+                            <h3 class="text-small-h3 lg:text-h3 font-bold text-brand-white mb-5">Applications</h3>
+                            <h2 class="text-small-h2 lg:text-h2 font-bold text-brand-white mb-5">Ready to bake</h2>
+                            <p class="hidden md:block text-brand-white w-6/12 mb-5 text-small-p lg:text-p">It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                            <a href="#" title="View recipes" aria-label="View recipes" tabindex="0" class="p-2 text-small-p md:p-4 md:text-p bg-brand-light-green rounded-lg inline-block transition-all group duration-300">View recipes
+                                <span class="block max-w-0 group-hover:max-w-full transition-all duration-500 h-0.5 bg-brand-black"></span>
+                            </a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
---
+++ data/sectors.php
@@ -0,0 +1,6 @@
+<?php
+$sectors = [
+    ['url' => '#', 'title' => 'Manufacturing', 'img' => 'images/Manufacturing.jpeg'],
+    ['url' => '#', 'title' => 'Foodservice', 'img' => 'images/foodservice.jpg'],
+    ['url' => '#', 'title' => 'Retail', 'img' => 'images/retail.jpg'],
+];
